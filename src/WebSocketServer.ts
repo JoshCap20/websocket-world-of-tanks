cc18192af1d33f29a74d08458b0b932a71c0b73c
Josh Caponigro <joshcap@email.unc.edu>
2024-03-22 04:21:41 -0400

    Startup tweaks

--- src/WebSocketServer.ts
+++ src/WebSocketServer.ts
@@ -8,15 +8,22 @@ export class WebSocketServer {
     private game: Game;
 
     constructor(server: http.Server) {
+        // TODO: Implement WebSocket compression: https://github.com/websockets/ws?tab=readme-ov-file#websocket-compression
         this.wss = new WebSocket.Server({ server });
         this.game = new Game(this.wss);
 
         this.wss.on('connection', (ws: WebSocket) => {
+            this.logNewConnection(ws);
             this.game.addPlayer(ws);
 
             ws.on('message', (message: string) => {
                 this.game.handleSignal(ws, message);
             });
         });
+    }
+
+    private logNewConnection(ws: WebSocket) {
+        // TODO: Implement logging
+        console.log('New connection from', ws.url);
     }
 }
