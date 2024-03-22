9f4c0b37cd6ee3e614876cf0cbaab6545c2d53f0
Josh Caponigro <joshcap@email.unc.edu>
2024-03-22 03:53:47 -0400

    Game refactoring, finish addPlayer logic with unicast

--- src/Game.ts
+++ src/Game.ts
@@ -2,7 +2,7 @@ import WebSocket, { Server } from 'ws';
 
 import { generateRandomObstacles, generatePlayerId, generateRandomPosition } from './utils/generators';
 import { collidesWithObstacle, collidesWithTank } from './utils/collisions';
-import { generateDestroyedPayload, generateLevelUpPayload } from './utils/signals';
+import { generateDestroyedPayload, generateLevelUpPayload, generatePlayerJoinedPayload } from './utils/signals';
 import { Tank } from './models/Tank';
 import { Obstacle } from './models/Obstacle';
 import { Bullet } from './models/Bullet';
@@ -17,69 +17,56 @@ TODO:
 export class Game {
     private wss: Server;
     private gameState: GameState;
-    private aiTanks: Map<string, Tank>;
+    private aiTanks: string[];
 
     constructor(wss: Server) {
         this.wss = wss;
         this.gameState = this.generateGameState();
-        this.aiTanks = new Map();
-    }
-
-    generateMapSize(): { height: number, width: number } {
-        return {
-            height: Math.random() * 1000 + 1000,
-            width: Math.random() * 1000 + 1000,
-        };
-    }
-
-    generateGameState(): GameState {
-        const mapSize = this.generateMapSize();
-        return {
-            mapSize: {
-                height: mapSize.height,
-                width: mapSize.width,
-            },
-            gameObjects: {
-                tanks: new Map(),
-                bullets: new Map(),
-                obstacles: generateRandomObstacles(mapSize.width, mapSize.height),
-            }
-        };
+        this.aiTanks = []; // IDs of AI tanks
     }
 
     // Core Player Management Methods (should refactor to a separate Player Management class eventually)
-    addPlayer(ws: WebSocket): Tank {
+    public addPlayer(ws: WebSocket): void {
         const playerId: string = generatePlayerId();
         const newPlayer: Tank = this.generateTank(playerId);
         this.gameState.gameObjects.tanks.set(playerId, newPlayer);
-        return newPlayer;
+        this.unicastPlayerJoined(ws, playerId);
     }
 
-    removePlayer(playerId: string): void {
+    public removePlayer(playerId: string): void {
         this.gameState.gameObjects.tanks.delete(playerId);
     }
 
     // Core Signal Sending Methods (should refactor to a separate Signal Gateway class eventually)
-    broadcastGameState(): void {
+    private unicast(ws: WebSocket, payload: string): void {
+        ws.send(payload);
+    }
+
+    private broadcast(payload: string): void {
         this.wss.clients.forEach((client: WebSocket) => {
-            client.send(JSON.stringify(this.gameState));
+            client.send(payload);
         });
     }
 
-    broadcastPlayerLevelUp(player: Tank): void {
-        this.wss.clients.forEach((client: WebSocket) => {
-            client.send(JSON.stringify(generateLevelUpPayload(player.id, player.level)));
-        });
+    protected broadcastGameState(): void {
+        this.broadcast(JSON.stringify(this.gameState));
     }
 
-    broadcastPlayerDeath(playerId: string): void {
-        this.wss.clients.forEach((client: WebSocket) => {
-            client.send(JSON.stringify(generateDestroyedPayload(playerId)));
-        });
+    protected broadcastPlayerLevelUp(player: Tank): void {
+        this.broadcast(JSON.stringify(generateLevelUpPayload(player.id, player.level)));
     }
+
+    protected broadcastPlayerDeath(playerId: string): void {
+        this.broadcast(JSON.stringify(generateDestroyedPayload(playerId)));
+    }
+
+    protected unicastPlayerJoined(ws: WebSocket, playerId: string): void {
+        this.unicast(ws, JSON.stringify(generatePlayerJoinedPayload(playerId)));
+    }
+
 
     // Tank Factory Method
-    generateTank(playerId: string): Tank {
+    private generateTank(playerId: string): Tank {
         const spawnPosition = generateRandomPosition(this.gameState.mapSize.width, this.gameState.mapSize.height, 50, 50, this.gameState.gameObjects.obstacles);
         return {
             id: playerId,
@@ -99,15 +86,38 @@ export class Game {
     }
 
     // Collision Detection Methods (should refactor to a separate Physics class eventually)
-    checkCollision(tank: Tank): boolean {
-        return this.#checkCollisionWithObstacles(tank.x, tank.y, tank.width, tank.height) || this.#checkCollisionWithTanks(tank.x, tank.y, tank.width, tank.height, tank.id);
+    protected checkCollision(tank: Tank): boolean {
+        return this.checkCollisionWithObstacles(tank.x, tank.y, tank.width, tank.height) || this.checkCollisionWithTanks(tank.x, tank.y, tank.width, tank.height, tank.id);
     }
 
-    #checkCollisionWithObstacles(x: number, y: number, width: number, height: number): boolean {
+    private checkCollisionWithObstacles(x: number, y: number, width: number, height: number): boolean {
         return collidesWithObstacle(x, y, width, height, this.gameState.gameObjects.obstacles);
     }
 
-    #checkCollisionWithTanks(x: number, y: number, width: number, height: number, playerId: string): boolean {
+    private checkCollisionWithTanks(x: number, y: number, width: number, height: number, playerId: string): boolean {
         return collidesWithTank(x, y, width, height, this.gameState.gameObjects.tanks, playerId);
+    }
+
+    // Map and Game Factory Methods
+    private generateMapSize(): { height: number, width: number } {
+        return {
+            height: Math.random() * 1000 + 1000,
+            width: Math.random() * 1000 + 1000,
+        };
+    }
+
+    private generateGameState(): GameState {
+        const mapSize = this.generateMapSize();
+        return {
+            mapSize: {
+                height: mapSize.height,
+                width: mapSize.width,
+            },
+            gameObjects: {
+                tanks: new Map(),
+                bullets: new Map(),
+                obstacles: generateRandomObstacles(mapSize.width, mapSize.height),
+            }
+        };
     }
 }
